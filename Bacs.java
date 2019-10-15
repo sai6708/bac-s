703be7e65738aa2daa7ea34745028a4105920e4f
saiteja <steja6708@gmail.com>
2019-10-15 16:20:18 +0530

    Update Bacs.java

--- Bacs.java
+++ Bacs.java
@@ -1,7 +1,3 @@
-
-package bacs;
-
-
 import java.util.Scanner;
 import java.lang.*; 
 
@@ -29,11 +25,11 @@ public class Bacs {
 	public static String NumberRand()
 	{
 	    String basenum = "123456789", finalnum="", tempstr="";
-	    Integer ranum = 0, max = 8 ;
+	    Integer ranum = 0, max = 8, compguess_len = 4 ;
             StringBuilder sb = new StringBuilder();
 
             tempstr = basenum;
-            for (int i=1;i<=4;i++)
+            for (int i=1;i<=compguess_len;i++)
             {
                 sb.setLength(0);
                 ranum = (int )(Math.random() * max + 1);
@@ -44,7 +40,7 @@ public class Bacs {
                 max = max - 1;
             }
 
-           return finalnum;
+        return finalnum;
 	}
 	
 	
@@ -52,22 +48,39 @@ public class Bacs {
 	{
             char[] temp = Userguess.toCharArray(); 
             Integer bulls=0, cows=0;
-            
-	    if(!Compguess.equals(Userguess))
-            {
+			
+            int num_duplicate[]=new int[10];
+			
+	    if(!Compguess.equals(Userguess)){
                 for (int i = 0; i < temp.length; i++) { 
-                    
-                    if (temp[i] == Compguess.charAt(i))
-                        bulls++;
-                    else if ((Compguess.indexOf(String.valueOf(Userguess.charAt(i))))>=0)
-                        cows++;
-                }
-                System.out.println(bulls+" bulls, "+cows+" cows");
-            }
-	    else
-            {
+					
+					if(num_duplicate[temp[i]]>1)
+					{	
+						System.out.println("Input has Duplicate Entries ");
+						bulls=0; 
+						cows=0;
+						break;
+						
+					}
+					else{
+						 if (temp[i] == Compguess.charAt(i))
+							bulls++;
+							num_duplicate[temp[i]]++;
+						else if ((Compguess.indexOf(String.valueOf(Userguess.charAt(i))))>=0)
+							cows++;
+					}
+                   
+                } 
+				
+				if(bulls>0 || cows>0)
+				{
+					System.out.println(bulls+" bulls, "+cows+" cows");
+				}
+                
+        }
+	    else{
                 System.out.println("won");
-            }
+        }
             
             
 	}
